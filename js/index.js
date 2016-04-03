65259633f70d2dc1440699c4383449edf7f05b8b
buzz69 <sebastien.masson.pro@gmail.com>
2016-04-03 15:44:18 +0200

    Change mp3 path and try to improve error details

--- js/index.js
+++ js/index.js
@@ -4,7 +4,7 @@ $( document ).bind( "deviceready", function() {
 });
 
 function playsound(mp3File) {
-	filePath = 'res/sounds/' + mp3File;
+	filePath = 'sounds/' + mp3File;
 	$('#statusBar').html('Path: ' + filePath);
 	//var audioElement = document.getElementById(id);
 	//var url = audioElement.getAttribute('src');
@@ -16,9 +16,34 @@ function playsound(mp3File) {
 		// error callback
 		function (err) { 
 			currentContent=$('#statusBar').html();
-			$('#statusBar').html(currentContent + '<br>' + 'PlayAudio error: ' + err);
+			$('#statusBar').html(currentContent + '<br>' + 'PlayAudio error: ' + dump(err));
 		}
 	);
   // Play audio
   my_media.play();
+}
+
+function dump(arr,level) {
+	var dumped_text = "";
+	if(!level) level = 0;
+	
+	//The padding given at the beginning of the line.
+	var level_padding = "";
+	for(var j=0;j<level+1;j++) level_padding += "    ";
+	
+	if(typeof(arr) == 'object') { //Array/Hashes/Objects 
+		for(var item in arr) {
+			var value = arr[item];
+			
+			if(typeof(value) == 'object') { //If it is an array,
+				dumped_text += level_padding + "'" + item + "' ...\n";
+				dumped_text += dump(value,level+1);
+			} else {
+				dumped_text += level_padding + "'" + item + "' => \"" + value + "\"\n";
+			}
+		}
+	} else { //Stings/Chars/Numbers etc.
+		dumped_text = "===>"+arr+"<===("+typeof(arr)+")";
+	}
+	return dumped_text;
 }
