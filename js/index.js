e76c29217740bd705dfa3c585cf06c202386a638
buzz69 <sebastien.masson.pro@gmail.com>
2016-04-03 20:38:04 +0200

    Try to fix duration error

--- js/index.js
+++ js/index.js
@@ -1,15 +1,23 @@
 var my_media=null;
 var mediaTimer=null;
+var playing = false;
 
 $( document ).bind( "deviceready", function() {
 	
 });
 
 function playSound(mp3File) {
+	if( my_media != null ){
+		my_media.stop();
+		my_media.release();
+		clearInterval(mediaTimer);
+		mediaTimer = null; 
+		my_media = null;
+		playing = true; 
+	}
+	
 	filePath = '/android_asset/www/sounds/' + mp3File;
-	//var audioElement = document.getElementById(id);
-	//var url = audioElement.getAttribute('src');
-	var my_media = new Media(filePath,
+	var my_media = new Media(filePath,onEnd,
 		// success callback
 		function () {
 			console.log("playAudio():Audio Success");
@@ -19,8 +27,25 @@ function playSound(mp3File) {
 			$('#statusBar').html('PlayAudio error: ' + dump(err));
 		}
 	);
+	
+	var duration = 0;
+	var counter = 0;
+	var timerDur = setInterval( function() {
+		counter = counter + 100;
+		if (counter > 2000) {
+			clearInterval(timerDur);
+		}
+		var dur = my_media.getDuration();
+		if (dur > 0) {
+			clearInterval(timerDur);
+		   duration = dur; 
+		}
+    }, 100);
+	
 	// Play audio
 	my_media.play();
+	
+	// progress
 	var mediaTimer = setInterval(function () {
 		duration=my_media.getDuration();
 		// get media position
@@ -43,6 +68,14 @@ function playSound(mp3File) {
 			}
 		);
 	}, 1000);
+	
+	function onEnd() {
+		if( my_media && playing == false ){
+			$('.stop-button').removeClass('stop-button');
+			$('.progressinternal').css('width', '3%');
+			my_media.release();
+		}
+	}
 }
 
 function dump(arr,level) {
