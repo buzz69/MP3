6e5d771c6a805fee759930e4fe21e856f9a5f703
buzz69 <sebastien.masson.pro@gmail.com>
2016-04-26 00:41:47 +0200

    Test some design change and title animation

--- js/index.js
+++ js/index.js
@@ -113,20 +113,20 @@ function generateButtons(category){
 	$('#mainDiv').html('');
 	if(category=='HOME'){
 		counter=0;
-		content='<div class="ui-grid-b">';
+		content='</br><div class="ui-grid-b">';
 		for(categorie in mediasTab){
 			counter++;
 			if(counter==1 || counter==4 || counter==7 || counter==10 || counter==13 || counter==16 || counter==19 || counter==22 || counter==25 || counter==28 || counter==31 || counter==34 || counter==37 || counter==40){content+='<div class="ui-block-a">';}
 			if(counter==2 || counter==5 || counter==8 || counter==11 || counter==14 || counter==17 || counter==20 || counter==23 || counter==26 || counter==29 || counter==32 || counter==35 || counter==38 || counter==41){content+='<div class="ui-block-b">';}
 			if(counter==3 || counter==6 || counter==9 || counter==12 || counter==15 || counter==18 || counter==21 || counter==24 || counter==27 || counter==30 || counter==33 || counter==36 || counter==39 || counter==42){content+='<div class="ui-block-c">';}
-			content+='<center><img width=100 src="'+mediasTab[categorie]['thumbnail']+'" onclick="generateButtons(\''+categorie+'\');"/><p>'+mediasTab[categorie]['titre']+'</p></center></div>';
+			content+='<center><img width=100 src="'+mediasTab[categorie]['thumbnail']+'" onclick="generateButtons(\''+categorie+'\');"/><p class="category-title">'+mediasTab[categorie]['titre']+'</p></center></div>';
 		}
 		content+='</div>';
 		$('#mainDiv').html(content);
 	}else{
 		for(categorie in mediasTab){
 			if(categorie==category){
-				$("#mainDiv").append('<div class="ui-grid-solo"><div class="ui-block-a"><img src="'+mediasTab[categorie]['banner']+'"/></div></div>');
+				$("#mainDiv").append('<div class="ui-grid-solo"><div class="ui-block-a" id="banner-wrapper"><img src="'+mediasTab[categorie]['banner']+'"/></div></div>');
 				$("#mainDiv").append("<ul id='mp3list' data-role='listview' data-inset='true'></ul>");
 				$("#mainDiv").trigger("create");
 				for(mp3Title in mediasTab[categorie]){
